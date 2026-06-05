418e752459e7774fac4b66f820f37d282cd4be9b
AlexMihalache99 <alexandrumihalache39@gmail.com>
2026-06-05 21:50:02 +0300

    add health endpoint

---
+++ src/main/java/com/alexandru/store_management_api/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.alexandru.store_management_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
