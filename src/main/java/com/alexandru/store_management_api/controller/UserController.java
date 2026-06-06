6a5bc8d92b2137cc614b84129b6dfdcdf8bd0795
AlexMihalache99 <alexandrumihalache39@gmail.com>
2026-06-06 19:31:02 +0300

    add UserController

--- src/main/java/com/alexandru/store_management_api/controller/UserController.java
+++ src/main/java/com/alexandru/store_management_api/controller/UserController.java
@@ -1,5 +1,44 @@
 package com.alexandru.store_management_api.controller;
 
+import com.alexandru.store_management_api.entity.User;
+import com.alexandru.store_management_api.service.UserService;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
-    
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        return userService.updateUser(id, updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
 }
