7a1d68b2a503357ffcca79dbeb1232363c82ff81
AlexMihalache99 <alexandrumihalache39@gmail.com>
2026-06-06 19:28:13 +0300

    add UserService

--- src/main/java/com/alexandru/store_management_api/service/UserService.java
+++ src/main/java/com/alexandru/store_management_api/service/UserService.java
@@ -1,5 +1,45 @@
 package com.alexandru.store_management_api.service;
 
+import com.alexandru.store_management_api.entity.User;
+import com.alexandru.store_management_api.repository.UserRepository;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User updateUser(Long id, User updatedUser) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(updatedUser.getUsername());
+                    user.setRole(updatedUser.getRole());
+                    return userRepository.save(user);
+                })
+                .orElse(null);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
     
 }
